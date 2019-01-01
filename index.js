360767089c403a7bab5a61acd1aa1f25dd6659a6
shawn a meighn <smeighn@gmail.com>
2019-01-01 20:16:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+var cart;
+
+function setCart(newCart) {
+  cart = newCart;
+}
+
+function total() {
+  let t = 0
+
+  for (var i = 0, l = cart.length; i < l; i++) {
+    for (var item in cart[i]) {
+      t += cart[i][item]
+    }
+  }
+
+  return t
+}
+  
 }
 
 function viewCart() {
